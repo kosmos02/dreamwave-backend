b9343ee4227e96f42f316191b86a1040728dc9f8
kosmos02 <alexandrgabe@gmail.com>
2021-05-09 19:43:45 -0600

    fixed post request backend

--- seeds/01-add-dreams.js
+++ seeds/01-add-dreams.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('dreams').insert([
-        {date: '03/20/2021', lucid: true, type: 'positive', tagarray: ['flying', 'orchestra', 'ice cream'], details: 'I was soaring above the clouds having myself a strawberry ice cream'},
-        {date: '04/02/2021', lucid: false, type: 'neutral', tagarray: ['swamp', 'playing cards', 'sunset'], details: 'It was sunset and and I was floating in a small raft in a swamp. I kept finding different cards laying around the raft.'},
-        {date: '04/20/2021', lucid: false, type: 'nightmare', tagsarray: ['mice', 'glue'], deatils: "There was a small and big mouse. The small mouse ate some super glue and then the big mouse went inside the small mouse's mouth. We can assume what happens next."}
+        {date: '03/20/2021', lucid: true, type: 'positive', tagsarray: ['flying', 'orchestra', 'ice cream'], details: 'I was soaring above the clouds having myself a strawberry ice cream'},
+        {date: '04/02/2021', lucid: false, type: 'neutral', tagsarray: ['swamp', 'playing cards', 'sunset'], details: 'It was sunset and and I was floating in a small raft in a swamp. I kept finding different cards laying around the raft.'},
+        {date: '04/20/2021', lucid: false, type: 'nightmare', tagsarray: ['mice', 'glue'], details: "There was a small and big mouse. The small mouse ate some super glue and then the big mouse went inside the small mouse's mouth. We can assume what happens next."}
       ]);
     });
 };
--- server.js
+++ server.js
@@ -10,33 +10,59 @@ app.use(cors());
 app.use( bodyParser.urlencoded({ extended: false }) )
 app.use( bodyParser.json() );
 
-const candies = [
-    {id: 1, name: 'Skittles'},
-    {id: 2, name: 'Starburst'},
-    {id: 3, name: 'Reeses'}
-]
+const knex = require('knex');
+const config = require('./knexfile')['development'];
+const database = knex(config);
 
-app.get('/', (_, response, next) => {
-    response.send({ message: "hooray!"})
+app.listen(PORT, () => console.log('Listening on port 4000'))
+
+app.get('/dreams', (_, response) => {
+    database('dreams')
+        .then(dreams => response.json(dreams))
 })
 
-app.get('/candies', (_, response, next) => {
-    response.json(candies)
+app.get('/dreams/:id', (request, response) => {
+    database('dreams')
+        .where({ id: request.params.id})
+        .then(dream => response.json(dream[0]));
 })
 
-app.get('/candies/:id', (request, response) => {
-
-    const candy =
-        candies.find(candy => candy.id === +request.params.id)
-    response.json(candy)
+app.post('/dreams', (request, response) => {
+    database('dreams')
+        .insert({ 
+            date: request.body.date, 
+            lucid: request.body.lucid, 
+            type: request.body.type, 
+            tagsarray: request.body.tagsarray, 
+            details: request.body.details
+        })
+        .then(dream => response.json(dream[0]))
 })
 
-app.post('/candies', (request, response) => {
-    const candy = {id: 4, name: request.body.name}
-    candies.push(candy)
-    response.json(candy)
-})
+// const candies = [
+//     {id: 1, name: 'Skittles'},
+//     {id: 2, name: 'Starburst'},
+//     {id: 3, name: 'Reeses'}
+// ]
 
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-})
+// app.get('/', (_, response, next) => {
+//     response.send({ message: "hooray!"})
+// })
+
+// app.get('/candies', (_, response, next) => {
+//     response.json(candies)
+// })
+
+// app.get('/candies/:id', (request, response) => {
+
+//     const candy =
+//         candies.find(candy => candy.id === +request.params.id)
+//     response.json(candy)
+// })
+
+// app.post('/candies', (request, response) => {
+//     const candy = {id: 4, name: request.body.name}
+//     candies.push(candy)
+//     response.json(candy)
+// })
+
